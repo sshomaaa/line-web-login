a2ebc4b318675fe117e6b4e751000c2a8475b9ad
sshomaaa <sshomaaa1356@gmail.com>
2023-08-30 12:26:09 +0900

    fix

--- execserver.sh
+++ execserver.sh
@@ -4,6 +4,7 @@ cd $(dirname $0)
 docker build --file=- --tag=line-web-login:latest . <<EOF
 FROM node:18.17.1-bookworm
 RUN groupadd -g $(id -g) user && useradd -m -s /bin/bash -u $(id -u) -g $(id -g) user; exit 0
+RUN mkdir /.npm && chown $(id -g):$(id -u) /.npm
 USER $(id -u)
 WORKDIR /src
 RUN git clone https://github.com/sshomaaa/line-web-login.git app
@@ -15,7 +16,6 @@ EOF
 docker run --rm \
     -it \
 	--user "$(id -u):$(id -g)" \
-	-v "$(pwd):/src" \
     -e "LINE_LOGIN_CHANNEL_ID=${LINE_LOGIN_CHANNEL_ID}" \
     -e "LINE_LOGIN_REDIRECT_URI=${LINE_LOGIN_REDIRECT_URI}" \
     -p "9000:9000" \
